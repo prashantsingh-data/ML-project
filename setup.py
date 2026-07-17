e76a188dda2094c3f249682874ae9192b29297ca
Prashant Singh <prashantsingh5662@gamil.com>
2026-07-17 21:21:01 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages,setup 
+
+
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='Prashant',
+author_email='prashantsingh5662@gmail.com',
+packages=find_packages(),
+install_requires=['pandas','numpy','seaborn']    
+
+
+
+
+)

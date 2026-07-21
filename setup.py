ff36c05e44caa6b7a6c4826107464821c020eeb0
Prashant Singh <prashantsingh5662@gamil.com>
2026-07-21 23:35:15 +0530

    initial commit

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages,setup 
 from typing import List
 
-HYPEN_E_DOT='-e .'
+HYPHEN_E_DOT= "-e ."
 def get_requirements(file_path:str)->List[str]:
     '''
     this function will return the list of requirements
@@ -11,8 +11,8 @@ def get_requirements(file_path:str)->List[str]:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n","") for req in requirements]
 
-        if HYPEN_E_DOTin requirements:
-            requirements.remove(HYPEN_E_DOT)
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
 
     return requirements
 
@@ -22,7 +22,7 @@ version='0.0.1',
 author='Prashant',
 author_email='prashantsingh5662@gmail.com',
 packages=find_packages(),
-install_requires=get_requirements('requirement.txt')  
+install_requires=get_requirements('requirements.txt')  
 
 
 
